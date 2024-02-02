b46280a13ddc3e44caf7376abbafafa30743fa43
muhammadraazy <muhammadrazi51@gmail.com>
2024-02-02 07:34:58 -0800

    fixed the banner on dekstop size

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,16 +1,17 @@
 "use client"
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from "next/image"
-import { banner, mobileBanner } from "@/assets"
+import { banner } from "@/assets"
 
 const Banner = () => {
-   const isWindow = typeof window != "undefined";
-  //  isWindow && window.innerWidth < 700 ? banner : mobileBanner
-  console.log(isWindow)
 
   return (
-    <div className="w-full xl:h-[280px] h-auto border-[2px] mb-[40px] border-[#000] rounded-[15px] overflow-hidden">
-        <Image src={banner} alt="banner" className="max-w-[100%] xl:h-full h-auto object-cover" />
+    <div className="w-full xl:h-[280px] h-auto mb-[40px] rounded-[15px] overflow-hidden">
+        <Image 
+          src={banner} 
+          alt="banner" 
+          className="max-w-[100%] h-auto object-cover" 
+        />
     </div>
   )
 }
--- src/components/dashboard/InputField.tsx
+++ src/components/dashboard/InputField.tsx
@@ -13,7 +13,7 @@ const InputField: FC<IProps> = ({ handleInput, handleChange, value }) => {
   //#44298c
   return (
     <fieldset className="lg:w-[100%] w-[100%] bg-bg-card h-[85px] mt-[15px] overflow-hidden rounded-[15px]">
-        <legend className="text-[#fff] w-full text-center font-bold md:text-[16px] text-[14px]"> Check your rewards </legend>
+        <legend className="text-[#fff] w-full text-center translate-y-[20px] font-bold md:text-[16px] text-[14px]"> Check your rewards </legend>
         <input
             className="p-[10px] text-[#7c6baa] input-field bg-bg-card border-[2px] border-[#353853] focus:border-[#44298c] rounded-[15px] placeholder:text-center placeholder:text-[#7c6baa] placeholder:font-semibold focus:placeholder:text-[#fff] text-center font-semibold outline-none w-full h-full"
             placeholder='Paste wallet' 
