8326e5a329cf6071b3c8eebd3814ec8af4e95081
muhammadraazy <muhammadrazi51@gmail.com>
2024-02-13 10:43:55 -0800

    changing a loading logo

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from "next/image"
-import {logo} from "@/assets"
+import {solgrow} from "@/assets"
 
 const LoadingSkeleton = () => {
   return (
     <div className="h-screen fixed top-0 z-[9] w-[100vw] loading text-[#000]">
         <div className="flex w-[100%] h-full flex-col justify-center items-center gap-[20px]">
           <h2 className="text-center text-[18px] text-[#fff] font-semibold"> please wait </h2>
-          <Image src={logo} alt="loading-logo" className="w-[50px] h-[50px] spin-logo rounded-[15px]" />
+          <Image src={solgrow} alt="loading-logo" className="w-[50px] h-[50px] spin-logo rounded-[15px]" />
         </div>
     </div>
   )
